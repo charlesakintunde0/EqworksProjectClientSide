1467b410b1de26a94c7a9fde2eef331b91a31295
Ireoluwa0011 <99848171+Ireoluwa0011@users.noreply.github.com>
2022-07-30 09:24:26 -0300

    Map Intergration

--- src/reducers/events_per_day.js
+++ src/reducers/events_per_day.js
@@ -1,6 +1,5 @@
 import { FETCH_EVENTS_PER_DAY } from "../constants/actionTypes";
 
-
 export default (events_per_day = [], action) => {
     switch (action.type) {
         case FETCH_EVENTS_PER_DAY:
